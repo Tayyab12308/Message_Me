0519b6fecd8ec36c5d911aba2e9c8bc0937c3857
Tayyab Iqbal <tayyab12308@gmail.com>
2019-01-19 23:55:44 +0000

    display messages in real time, fix display and add user signup from UI

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,8 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(username: params[:sessin][:username])
-    if user && user.authenticate(params[:sessin][:password])
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:success] = "You have successfully logged in"
       redirect_to root_path
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
   
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+  resources :users, except: [:new]
+  
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
   post 'message', to: 'messages#create'
+  
+  mount ActionCable.server, at: '/cable'
 end
