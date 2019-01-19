be552c660c697ece1c8dae9ebceeadba01113fa9
Tayyab Iqbal <tayyab12308@gmail.com>
2019-01-19 13:23:32 +0000

    add sessions create and destroy and enable flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,25 @@
 class SessionsController < ApplicationController
+
   def new
     
   end
+  
+  def create
+    user = User.find_by(username: params[:sessin][:username])
+    if user && user.authenticate(params[:sessin][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Username or password were incorrect"
+      render 'new'
+    end
+  end
+  
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out"
+    redirect_to login_path
+  end
+
 end
